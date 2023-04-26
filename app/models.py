55a01acdf94136730714c28df3440dd7c95cd359
Agu Sunday <sndyaguu@gmail.com>
2023-04-26 23:45:34 +0100

    Commit up to Episode 13

--- app/models.py
+++ app/models.py
@@ -1,9 +1,14 @@
 from app import app
 
-from app import db
+from app import db, login_manager
 from app import bcrypt
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False)
     street = db.Column(db.String(length=30), nullable=False)
@@ -26,12 +31,23 @@ class User(db.Model):
     courses = db.relationship('Course', backref='owned_user', lazy=True)
 
     @property
+    def prettier_balance(self):
+        if len(str(self.balance)) >= 4:
+            return f'{str(self.balance)[:-3]},{str(self.balance)[-3:]}'
+        else:
+            return f"{self.budget}"
+
+    @property
     def password(self):
         return self.password
     
     @password.setter
     def password(self, plain_text_password):
         self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+    
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
 
 class Course(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, url_for,flash
 from app.models import Course, User
 from app.forms import ApplyForm, LoginForm
 from app import db
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route("/")
@@ -35,6 +36,9 @@ def apply_page():
                               )
         db.session.add(user_to_create)
         db.session.commit()
+
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('after_apply_page'))
     if form.errors != {}:
         for err_msg in form.errors.values():
@@ -42,10 +46,27 @@ def apply_page():
     return render_template('apply.html', form=form)
 
 @app.route("/after_apply")
+@login_required
 def after_apply_page():
     return render_template('after_apply.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('after_apply_page'))
+        else:
+            flash('Usename and password are not match! Please try again', category='danger')
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home_page"))
